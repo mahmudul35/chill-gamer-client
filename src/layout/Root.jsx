348b2553d52b36c71dd630cf96ff310724b93b00
mahmudul35 <101060541+mahmudul35@users.noreply.github.com>
2024-12-04 23:46:40 +0600

    footer added

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 const Root = () => {
   return (
     <div className="container mx-auto">
       <Navbar />
-      <Outlet />
+      <div className="h-[calc(100vh-288px)]">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
