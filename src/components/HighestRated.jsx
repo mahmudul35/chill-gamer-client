2a0fd1e4ea8392cf2d379d5b2cc4aaae08770812
mahmudul35 <101060541+mahmudul35@users.noreply.github.com>
2025-01-07 16:36:07 +0600

    update highest reated section

--- src/components/HighestRated.jsx
+++ src/components/HighestRated.jsx
@@ -1,5 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
+import { MdStarRate } from "react-icons/md";
+import { SlCalender } from "react-icons/sl";
 import { Link } from "react-router-dom";
+import genre from "../assets/genre.png";
 import { AuthContext } from "../context/AuthProvider";
 const HighestRated = ({ dark }) => {
   const [reviews, setReviews] = useState([]);
@@ -24,7 +27,7 @@ const HighestRated = ({ dark }) => {
           <span className="loading loading-bars loading-lg text-green-500"></span>
         </div>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6 mt-8 dark:text-base-100">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-6 mt-8 dark:text-base-100">
           {reviews.map((review) => (
             <div key={review.id} className="card bordered">
               <figure>
@@ -37,10 +40,24 @@ const HighestRated = ({ dark }) => {
               <div className="card-body ">
                 <h2 className="card-title">{review.title}</h2>
 
-                <p>Rating: {review.rating}</p>
-                <p>Publising Year: {review.year.slice(0, 4)}</p>
-                <p>Genre: {review.genre}</p>
-                <Link to={`/reviewDetails/${review._id}`} className="btn">
+                <p>
+                  <span className="flex items-center gap-1">
+                    {" "}
+                    <MdStarRate className="text-yellow-600" />
+                    {review.rating}
+                  </span>{" "}
+                </p>
+                <p className="flex items-center gap-2">
+                  <SlCalender />
+                  {review.year.slice(0, 4)}
+                </p>
+                <p className="flex items-center gap-2">
+                  <img src={genre} className="w-5" alt="" /> {review.genre}
+                </p>
+                <Link
+                  to={`/reviewDetails/${review._id}`}
+                  className="btn bg-pink-800 text-white hover:bg-pink-700 hover:shadow-lg transition-all duration-300"
+                >
                   Explore Details
                 </Link>
               </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,25 @@
 import React, { useContext } from "react";
-
 import { NavLink } from "react-router-dom";
 import { Tooltip } from "react-tooltip";
 import "react-tooltip/dist/react-tooltip.css";
 import { AuthContext } from "../context/AuthProvider";
+
 const Navbar = () => {
   const { user, signOutUser, dark } = useContext(AuthContext);
 
   return (
-    <div className="navbar bg-base-100 dark:bg-slate-900 dark:text-white">
+    <div className="navbar bg-white  dark:bg-slate-900 dark:text-white">
+      {/* Navbar Start */}
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <button
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost lg:hidden"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
+              className="h-6 w-6"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -26,106 +31,92 @@ const Navbar = () => {
                 d="M4 6h16M4 12h8m-8 6h16"
               />
             </svg>
-          </div>
+          </button>
           <ul
-            tabindex="0"
-            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 w-52 p-2 rounded-lg bg-white shadow-lg dark:bg-slate-800"
           >
-            <NavLink to={"/"}>Home</NavLink>
-            <NavLink to="/allreview">All Reviews</NavLink>
-            <NavLink to="/addreview">Add Review</NavLink>
-            <NavLink to="/myreviews">My Reviews</NavLink>
-            <NavLink to="/watchlist">Game WatchList </NavLink>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/allreview">All Reviews</NavLink>
+            </li>
+            <li>
+              <NavLink to="/addreview">Add Review</NavLink>
+            </li>
+            <li>
+              <NavLink to="/myreviews">My Reviews</NavLink>
+            </li>
+            <li>
+              <NavLink to="/watchlist">Game WatchList</NavLink>
+            </li>
           </ul>
         </div>
-        <a className="hidden md:inline btn btn-ghost text-xl">Chill Gamer</a>
+        <NavLink
+          to="/"
+          className="hidden md:inline text-xl font-bold text-pink-800"
+        >
+          Chill Gamer
+        </NavLink>
       </div>
+
+      {/* Navbar Center */}
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 gap-7">
-          <NavLink
-            className={({ isActive, isPending }) =>
-              isPending
-                ? "pending"
-                : isActive
-                ? "text-slate-950 dark:text-red-700 font-bold hover:scale-105 underline "
-                : ""
-            }
-            to={"/"}
-          >
-            Home
-          </NavLink>
-          <NavLink
-            className={({ isActive, isPending }) =>
-              isPending
-                ? "pending"
-                : isActive
-                ? "text-slate-950 dark:text-red-700 font-bold hover:scale-105 underline "
-                : ""
-            }
-            to="/allreview"
-          >
-            All Reviews
-          </NavLink>
-          <NavLink
-            className={({ isActive, isPending }) =>
-              isPending
-                ? "pending"
-                : isActive
-                ? "text-slate-950 dark:text-red-700 font-bold hover:scale-105 underline "
-                : ""
-            }
-            to="/addreview"
-          >
-            Add Review
-          </NavLink>
-          <NavLink
-            className={({ isActive, isPending }) =>
-              isPending
-                ? "pending"
-                : isActive
-                ? "text-slate-950 dark:text-red-700 font-bold hover:scale-105 underline "
-                : ""
-            }
-            to="/myreviews"
-          >
-            My Reviews
-          </NavLink>
-          <NavLink
-            className={({ isActive, isPending }) =>
-              isPending
-                ? "pending"
-                : isActive
-                ? "text-slate-950 dark:text-red-700 font-bold hover:scale-105 underline "
-                : ""
-            }
-            to="/watchlist"
-          >
-            Game WatchList{" "}
-          </NavLink>
+        <ul className="menu menu-horizontal px-1 gap-5">
+          {[
+            { name: "Home", route: "/" },
+            { name: "All Reviews", route: "/allreview" },
+            { name: "Add Review", route: "/addreview" },
+            { name: "My Reviews", route: "/myreviews" },
+            { name: "Game WatchList", route: "/watchlist" },
+          ].map((item, index) => (
+            <li key={index}>
+              <NavLink
+                to={item.route}
+                className={({ isActive }) =>
+                  isActive
+                    ? "text-pink-800 font-semibold border-b-2 border-pink-800"
+                    : "hover:text-pink-800 transition-colors"
+                }
+              >
+                {item.name}
+              </NavLink>
+            </li>
+          ))}
         </ul>
       </div>
-      <div className="navbar-end">
+
+      {/* Navbar End */}
+      <div className="navbar-end flex items-center gap-4">
         {user && (
           <img
             src={user?.photoURL}
-            alt=""
-            className="h-10 w-10 rounded-full mr-4 my-anchor-element"
+            alt="User Avatar"
+            className="h-10 w-10 rounded-full border-2 border-pink-800 my-anchor-element"
           />
         )}
         <Tooltip anchorSelect=".my-anchor-element" place="top" className="z-50">
-          {user?.displayName}
+          {user?.displayName || "User"}
         </Tooltip>
         {user ? (
-          <button onClick={signOutUser} class="btn  bg-slate-800 text-white">
+          <button
+            onClick={signOutUser}
+            className="btn bg-pink-800 hover:bg-pink-700 text-white"
+          >
             Logout
           </button>
         ) : (
           <>
             <NavLink to="/auth/login">
-              <button class="btn  bg-slate-800 text-white mr-4">Login</button>
+              <button className="btn bg-pink-800 hover:bg-pink-700 text-white">
+                Login
+              </button>
             </NavLink>
             <NavLink to="/auth/register">
-              <button class="btn  bg-green-500 text-white">Register</button>
+              <button className="btn bg-green-500 hover:bg-green-400 text-white">
+                Register
+              </button>
             </NavLink>
           </>
         )}
